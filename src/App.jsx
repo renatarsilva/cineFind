94b3d3c7a8c3e6fefe0c3b928f2ef124afdad204
Renata <renata.rodriguesdasilva@outlook.com>
2025-02-13 18:39:08 -0300

    feat: Update Optimize search

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Search from "./components/search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -19,6 +20,9 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
